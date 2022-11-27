fce4504e1f4e852f1b621db35b71b7e71440820b
Edih Goodluck <77158925+Codablack23@users.noreply.github.com>
2022-11-27 20:12:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use(session({
     resave:false,
     saveUninitialized:false,
     proxy:true,
-    name:"api.bubbookz"
+    name:"api.bubbookz",
     store:new SequelizeStore({db:sequelize}),
     cookie:{
         httpOnly:true,
